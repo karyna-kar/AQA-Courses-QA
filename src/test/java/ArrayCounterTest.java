c7c3991bb601a1d8352ffda9c51346eae2709e34
Karina <karyna.karmaza@gmail.com>
2020-07-27 22:06:07 +0300

    Maven_TestNG

--- src/test/java/ArrayCounterTest.java
+++ src/test/java/ArrayCounterTest.java
@@ -7,18 +7,38 @@ public class ArrayCounterTest {
     private int [] testArrayCombine = new int []{ -1, -2, 0, 11, 6};
 
     @Test
-    public void findMax()
+    public void findMaxPosArray()
     {
         Assert.assertEquals(ArrayCounter.findMax(testArrayPositive), 10);
+    }
+
+    @Test
+    public void findMaxNegArray()
+    {
         Assert.assertEquals(ArrayCounter.findMax(testArrayNegative), -1);
+    }
+
+    @Test
+    public void findMaxCombArray()
+    {
         Assert.assertEquals(ArrayCounter.findMax(testArrayCombine), 11);
     }
 
     @Test
-    public void findMin()
+    public void findMinPosArray()
     {
         Assert.assertEquals(ArrayCounter.findMin(testArrayPositive), 5);
+    }
+
+    @Test
+    public void findMinNegArray()
+    {
         Assert.assertEquals(ArrayCounter.findMin(testArrayNegative), -5);
+    }
+
+    @Test
+    public void findMinCombArray()
+    {
         Assert.assertEquals(ArrayCounter.findMin(testArrayCombine), -2);
     }
 }
