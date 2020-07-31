acd0e1619915e388e3e0177d8d1ace1584398b1e
Karina <karyna.karmaza@gmail.com>
2020-07-31 19:00:13 +0300

    Changing methods names

--- src/test/java/ArrayCounterTest.java
+++ src/test/java/ArrayCounterTest.java
@@ -7,36 +7,36 @@ public class ArrayCounterTest {
     private int [] testArrayCombine = new int []{ -1, -2, 0, 11, 6};
 
     @Test
-    public void findMaxPosArray()
+    public void checkMaxPosArray()
     {
         Assert.assertEquals(ArrayCounter.findMax(testArrayPositive), 10);
     }
     @Test
-    public void findMaxNegArray()
+    public void checkMaxNegArray()
     {
         Assert.assertEquals(ArrayCounter.findMax(testArrayNegative), -1);
     }
 
     @Test
-    public void findMaxCombArray()
+    public void checkMaxCombArray()
     {
         Assert.assertEquals(ArrayCounter.findMax(testArrayCombine), 11);
     }
 
     @Test
-    public void findMinPosArray()
+    public void checkMinPosArray()
     {
         Assert.assertEquals(ArrayCounter.findMin(testArrayPositive), 5);
     }
 
     @Test
-    public void findMinNegArray()
+    public void checkMinNegArray()
     {
         Assert.assertEquals(ArrayCounter.findMin(testArrayNegative), -5);
     }
 
     @Test
-    public void findMinCombArray()
+    public void checkMinCombArray()
     {
         Assert.assertEquals(ArrayCounter.findMin(testArrayCombine), -2);
     }
