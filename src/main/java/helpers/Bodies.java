b970987890be2ead2f736ab52ba8b48c40a3c6e2
Karina_kar <karyna.karmaza@gmail.com>
2020-08-25 18:39:18 +0300

    LatestChanges

--- src/main/java/helpers/Bodies.java
+++ src/main/java/helpers/Bodies.java
@@ -3,8 +3,8 @@ package helpers;
 public class Bodies {
     public static String getRequestTokenBody()
     {
-        String client_id = "8723fa1b767933f1fc71";
-        String client_secret = "b34bf5221aec33a178ef07b5f449d9ad";
+        String client_id = "";
+        String client_secret = "";
 
         return "{\n" +
             "    \"client_id\": \""+client_id+"\", \n" +
