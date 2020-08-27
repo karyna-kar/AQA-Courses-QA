785cbb6653841e4b25a68b4f6437f05f8e68c56d
Karina_kar <karyna.karmaza@gmail.com>
2020-08-27 19:48:42 +0300

    JDBS

--- src/main/java/DBConnection/JDBCConnection.java
+++ src/main/java/DBConnection/JDBCConnection.java
@@ -1,14 +1,20 @@
 package DBConnection;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class JDBCConnection {
-    private static final String host = "WIN-TRATKJ7E1QN";
-    private static final String url = "jdbc:sqlserver://" + host + "/SQLEXPRESS_TEST";
+    private static final String host = "172.31.0.150";
+    private static final String url = "jdbc:sqlserver://" + host + ":1435;database=WorldEvents;";
     private static final String user = "Karina_test";
     private static final String password = "123";
+
+    private static final String connectionUrl =
+            "jdbc:sqlserver://172.31.0.150:1433;"
+                    + "database=WorldEvents;"
+                    + "user=Karina_test;"
+                    + "password=123;"
+                    + "trustServerCertificate=false;"
+                    + "loginTimeout=30;";
 
     private static Connection connection = null;
 
@@ -17,8 +23,19 @@ public class JDBCConnection {
             connection = DriverManager.getConnection(url, user, password);
         }
         catch(SQLException e){
-            System.out.println("Connection to DB failed!");
+            System.out.println("Connection to DB failed!"+e.getMessage());
         }
         return connection;
+    }
+
+    public static ResultSet selectFromDB(String query) {
+        ResultSet result =null;
+        try {
+            Statement stmt = connectToDB().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            result = stmt.executeQuery(query);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
     }
 }
--- src/test/java/TestQueries.java
+++ src/test/java/TestQueries.java
@@ -2,6 +2,9 @@ import DBConnection.JDBCConnection;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class TestQueries {
     @Test
     public void enableDBConnectionTest()
@@ -9,4 +12,12 @@ public class TestQueries {
        // Assertions.assertNotNull(JDBCConnection.connectToDB());
         Assert.assertNotNull(JDBCConnection.connectToDB());
     }
+    @Test (dependsOnMethods = {"enableDBConnectionTest"})
+    public void SelectRequestEventNameTest() throws SQLException {
+        String query = "SELECT * FROM [WorldEvents].[dbo].[tblEvent] where EventID = 1";
+        ResultSet rs = JDBCConnection.selectFromDB(query);
+        rs.first();
+        Assert.assertEquals(rs.getString("EventName"), "Chernobyl");
+    }
+
 }
